ff0559f8c48a2bb62b402bcb892da9738fa6ce2a
aaa <aaa>
2024-02-19 12:02:53 +0500

    feat: create component cart

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,6 +1,7 @@
 import Headling from '../../components/Headling/Headling';
 import Search from '../../components/Search/Search';
 import styles from './Menu.module.css';
+import ProductCard from '../../components/ProducrCard/ProductCard';
 
 export function Menu() {
 	return (<>
@@ -8,6 +9,17 @@ export function Menu() {
 			<Headling>Меню</Headling>;
 			<Search />
 		</div>
+		<div>
+			<ProductCard
+				id={1}
+				title='Наслаждение'
+				description='Салями, руккола, помидоры, оливки'
+				rating={4.5}
+				price={300}
+				image='/product-demo.png'
+			/>
+		</div>
+
 	</>
-	); 
+	);
 }
