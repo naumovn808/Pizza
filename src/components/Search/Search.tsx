77dd5b5988a51e32213dfc7e592d4221bf45ccbe
aaa <aaa>
2024-02-22 09:02:12 +0500

    create res to products

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -3,15 +3,16 @@ import styles from './Search.module.css';
 import cn from 'classnames';
 import { SearchProps } from './Search.props';
 
-const Search = forwardRef<HTMLInputElement, SearchProps>(function Input({ isValid = true, className, ...props }, ref) {
-	return (
-		<div className={styles['input-wrapper']}>
-			<input placeholder='Введите блюдо или состав' ref={ref} className={cn(styles['input'], className, {
-				[styles['invalid']]: isValid
-			})} {...props} />
-			<img className={styles['icon']} src='/search-icon.svg' alt='Иконка лупы' />
-		</div>
-	);
-});
+const Search = forwardRef<HTMLInputElement, SearchProps>(
+	function Input({ isValid = true, className, ...props }, ref) {
+		return (
+			<div className={styles['input-wrapper']}>
+				<input placeholder='Введите блюдо или состав' ref={ref} className={cn(styles['input'], className, {
+					[styles['invalid']]: isValid
+				})} {...props} />
+				<img className={styles['icon']} src='/search-icon.svg' alt='Иконка лупы' />
+			</div>
+		);
+	});
 
 export default Search;
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -2,22 +2,55 @@ import Headling from '../../components/Headling/Headling';
 import Search from '../../components/Search/Search';
 import styles from './Menu.module.css';
 import ProductCard from '../../components/ProducrCard/ProductCard';
+import { PREFIX } from '../../helpers/API';
+import { Product } from '../../interfaces/product.interface';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 export function Menu() {
+
+	const [products, setProducts] = useState<Product[]>([]);
+
+	const getMenu = async () => {
+		try {
+			const res = await fetch(`${PREFIX}/products`);
+
+			if (!res.ok) {
+				return;
+			}
+			const data = await res.json() as Product[];
+			setProducts(data);
+		} catch (e) {
+			console.error(e);
+			return;
+		}
+	};
+
+	useEffect(() => {
+		getMenu();
+	}, []);
+
+
 	return (<>
 		<div className={styles['head']}>
 			<Headling>Меню</Headling>;
 			<Search />
 		</div>
+
 		<div>
-			<ProductCard
-				id={1}
-				title='Наслаждение'
-				description='Салями, руккола, помидоры, оливки'
-				rating={4.5}
-				price={300}
-				image='/product-demo.png'
-			/>
+			{products.map(p => (
+				<ProductCard
+					key={p.id}
+					id={p.id}
+					name={p.name}
+					description={p.ingredients.join(', ')}
+					rating={p.rating}
+					price={p.price}
+					image={p.image}
+				/>
+
+			))}
+
 		</div>
 
 	</>
